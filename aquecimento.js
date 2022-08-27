571e5ffb9de0daea065e2836f684c5dc72ee20d3
KARINA GOMES MOREIRA <karina.gmoreira1@senacsp.edu.br>
2022-08-26 22:12:04 -0300

    CONDICIONAIS

---
+++ aquecimento.js
@@ -0,0 +1,56 @@
+
+/*
+for(let i = 0; i <= 10; i++) {
+    document.write(i + "<br>");
+}
+
+
+let i = 0;
+while (i < 10) {
+  i++;
+ document.write(i + "<br>");
+}
+
+
+let i = 0;
+do {
+  document.write(i +"<br>");
+   i++;
+} while (i <= 10);
+
+
+
+let i = 20;
+if (i > 5) {
+
+    document.write( i + " É MAIOR QUE 5");
+}else{
+
+    document.write(i + " É MENOR QUE 5");
+}
+
+
+let i = 1;
+switch(i){
+    case 0:
+    document.write("Amor");
+    break;
+    case 1:
+    document.write("Ódio");
+    break;
+    case 2:
+    document.write("Felicidade");
+    break;
+    case 3:
+    document.write("Paz");
+    break;
+
+}
+
+*/
+
+let genero = prompt('Entre com M ou F');
+
+let saida = genero ='M' ? 'Masculino' : 'Feminino';
+ 
+console.log(saida)
